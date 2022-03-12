c5412ea8b43f2feb2a42765f27162b63b755ac2b
yassir78 <yassir.acaf@gmail.com>
2022-03-12 17:50:02 +0100

    step_7

---
+++ src/main/java/utils/NumberToListConverter.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.List;
+
+public class NumberToListConverter {
+
+    public static List<Integer> convert(int number) throws IllegalArgumentException {
+        throw new IllegalArgumentException("Number cannot be 0");
+    }
+
+}
---
+++ src/test/java/utils/NumberToListConverterTest.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NumberToListConverterTest {
+
+    @Test
+    void shouldThrowExceptionWhenInputIs0() {
+        Executable executable = () -> NumberToListConverter.convert(0);
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                executable);
+        assertEquals("Number cannot be 0", exception.getMessage());
+    }
+}
